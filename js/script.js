5c013fc07088fb4f992dc1cc4991cd30d759db10
Hendrick Nkuba <115194843+leehendrick@users.noreply.github.com>
2023-12-12 12:52:08 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let currentIndex = 0;
+  
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach(slide => slide.classList.remove('show', 'next', 'prev'));
+
+  if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
+
+  slides[currentIndex].classList.add('show');
+  slides[(currentIndex + 1) % slides.length].classList.add('next');
+  slides[(currentIndex - 1 + slides.length) % slides.length].classList.add('prev');
+
+  updateIndicators();
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function createIndicators() {
+    const slides = document.querySelectorAll('.slide');
+    const indicatorsContainer = document.querySelector('.indicators');
+
+    slides.forEach((_, index) => {
+      const indicator = document.createElement('div');
+      indicator.classList.add('indicator');
+      indicator.addEventListener('click', () => showSlide(index));
+      indicatorsContainer.appendChild(indicator);
+    });
+
+    updateIndicators();
+  }
+
+  function updateIndicators() {
+    const indicators = document.querySelectorAll('.indicator');
+    indicators.forEach((indicator, index) => {
+      if (index === currentIndex) {
+        indicator.classList.add('active');
+      } else {
+        indicator.classList.remove('active');
+      }
+    });
+  }
+
+// Show the first slide on page load
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
+    createIndicators();
+})
+
+  // Adiciona transição automática a cada 5 segundos
+  setInterval(() => {
+    nextSlide();
+  }, 5000);
+
